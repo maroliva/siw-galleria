91b270d2a1d8c9fd3d02277be5370556cac218a4
Jerin George Mathew <jerin.edassery95@gmail.com>
2017-06-08 19:15:04 +0200

    modificata la classe nazione

--- src/main/java/it/uniroma3/siwgalleria/domain/Nazione.java
+++ src/main/java/it/uniroma3/siwgalleria/domain/Nazione.java
@@ -1,7 +1,27 @@
 package it.uniroma3.siwgalleria.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Nazione {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column
+    private String nomeNazione;
+
+    public Nazione() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNomeNazione() {
+        return nomeNazione;
+    }
+
+    public void setNomeNazione(String nomeNazione) {
+        this.nomeNazione = nomeNazione;
+    }
 }
