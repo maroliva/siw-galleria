58cdfb0cd039620bdbf2349a4f80f80b99bc61a9
marcoo195 <m.oliva1995@gmail.com>
2017-06-08 16:14:59 +0200

    avvio

---
+++ src/main/java/it/uniroma3/siwgalleria/controller/HomeController.java
@@ -0,0 +1,16 @@
+package it.uniroma3.siwgalleria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by marco on 08/06/2017.
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
