978813e6a46d0df62d745cc45d5a22f0229c630e
marcoo195 <m.oliva1995@gmail.com>
2017-06-08 15:15:46 +0200

    Primo commit

---
+++ src/main/java/it/uniroma3/siwgalleria/SiwGalleriaApplication.java
@@ -0,0 +1,12 @@
+package it.uniroma3.siwgalleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwGalleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwGalleriaApplication.class, args);
+	}
+}
